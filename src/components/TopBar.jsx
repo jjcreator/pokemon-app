62ee2567001f003acb95fd6d4e8a4e3a7ded6a1e
jjcreator <jjcreator8@gmail.com>
2022-09-17 23:10:50 +0200

    initial commit

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,15 @@
+
+import { Box } from "@mui/system";
+import DarkModeSwitch from "./DarkModeSwitch";
+import Search from "./Search";
+import styles from '../scss/topBar.module.scss'
+
+
+const TopBar = ({darkMode, handleDarkModeChange, findPokemonByName}) => 
+    
+    <Box bgcolor={`${darkMode === 'true' ? '#303031' : '#fefefe'}`} sx={{ width: '100%', display: 'flex', justifyContent: 'space-between', position: 'fixed', top: '0', left: 0, alignItems: 'center'}} className={styles.topBar}>
+        <Search findPokemonByName={ findPokemonByName }  darkMode={darkMode} />
+        <DarkModeSwitch handleDarkModeChange={handleDarkModeChange} />
+    </Box>
+
+export default TopBar
